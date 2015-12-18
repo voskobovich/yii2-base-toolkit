53ef190af9f2ca0212797c60cd09335a6b16de36
Vitaly Voskobovich <vitali.voskobovich@moonion.com>
2015-12-18 18:25:33 +0200

    Dev

--- db/ActiveQuery.php
+++ db/ActiveQuery.php
@@ -10,25 +10,13 @@ namespace voskobovich\base\db;
 class ActiveQuery extends \yii\db\ActiveQuery
 {
     /**
-     * Init
-     */
-    public function init()
-    {
-        parent::init();
-
-        /** @var \yii\db\ActiveRecord $tModel */
-        $tModel = new $this->modelClass;
-        $this->from(['t' => $tModel::tableName()]);
-    }
-
-    /**
      * Scope for model status
      * @param int $status
      * @return $this
      */
     public function status($status)
     {
-        $this->andWhere(['t.status_key' => $status]);
+        $this->andWhere(['status_key' => $status]);
         return $this;
     }
 
@@ -39,7 +27,7 @@ class ActiveQuery extends \yii\db\ActiveQuery
      */
     public function type($type)
     {
-        $this->andWhere(['t.type_key' => $type]);
+        $this->andWhere(['type_key' => $type]);
 
         return $this;
     }
