57567d7eb84056edae0818ad6feaf7f7f3709866
voskobovich <vitaly@voskobovich.com>
2016-01-24 10:00:21 +0200

    Dev

--- src/traits/ModelTrait.php
+++ src/traits/ModelTrait.php
@@ -4,6 +4,7 @@ namespace voskobovich\base\traits;
 
 use ReflectionClass;
 use yii\base\Model;
+use yii\db\ActiveRecord;
 
 
 /**
@@ -27,5 +28,32 @@ trait ModelTrait
         }
 
         return mb_strtolower($name) . '-form';
+    }
+
+    /**
+     * @param ActiveRecord $model
+     * @param string $defaultAttribute
+     * @param array $attributesMap
+     */
+    public function populateErrors(ActiveRecord $model, $defaultAttribute, $attributesMap = [])
+    {
+        /** @var Model $this */
+        $errors = $model->getErrors();
+
+        foreach ($errors as $attribute => $messages) {
+            $attribute = isset($attributesMap[$attribute])
+                ? $attributesMap[$attribute]
+                : $attribute;
+            if (false === $this->hasProperty($attribute)) {
+                if (!method_exists($this, 'hasAttribute')) {
+                    $attribute = $defaultAttribute;
+                } elseif (false === $this->hasAttribute($attribute)) {
+                    $attribute = $defaultAttribute;
+                }
+            }
+            foreach ($messages as $mes) {
+                $this->addError($attribute, $mes);
+            }
+        }
     }
 }
