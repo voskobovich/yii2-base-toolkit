1c9b01ad773dd594c7a0a298366f5c23228daab4
voskobovich <vitaly@voskobovich.com>
2016-02-06 19:13:51 +0200

    Dev

--- src/helpers/FileHelper.php
+++ src/helpers/FileHelper.php
@@ -10,12 +10,12 @@ namespace voskobovich\base\helpers;
 class FileHelper extends \yii\helpers\FileHelper
 {
     /**
-     * Генерация уникального имени файла в директории
+     * Generate unique file name in directory
      * @param $path
      * @param string $extension
      * @return string
      */
-    public static function getRandomFileName($path, $extension)
+    public static function generateRandomName($path, $extension)
     {
         if (strpos($extension, '.') !== 0) {
             $extension = '.' . $extension;
@@ -27,5 +27,38 @@ class FileHelper extends \yii\helpers\FileHelper
         } while (file_exists($file));
 
         return $name;
+    }
+
+    /**
+     * Download file from the link
+     * @param string $url
+     * @param string $path
+     * @param string $name
+     * @param string $extension
+     * @return bool|array
+     */
+    public static function saveFromUrl($url, $path, $name = null, $extension = 'png')
+    {
+        $file = file_get_contents(urldecode($url));
+        $extension = trim($extension, " \t\n\r\0\x0B.");
+
+        if (FileHelper::createDirectory($path)) {
+            return false;
+        }
+
+        if ($name == null) {
+            $name = FileHelper::generateRandomName($path, $extension);
+        } else {
+            $name = $name . '.' . $extension;
+        }
+
+        if (file_put_contents($path . DIRECTORY_SEPARATOR . $name, $file)) {
+            return [
+                'name' => $name,
+                'path' => $path
+            ];
+        }
+
+        return false;
     }
 }
