5fa94be0d9511e90de5cc69faffe6856994bfe57
voskobovich <vitaly@voskobovich.com>
2016-03-03 13:33:44 +0200

    Dev

---
+++ src/helpers/Cookies.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace voskobovich\base\helpers;
+
+use Yii;
+use yii\web\Cookie;
+
+
+/**
+ * Class Cookies
+ * @package voskobovich\base\helpers
+ */
+class Cookies
+{
+    /**
+     * Add cookie
+     * @param array|Cookie $cookie
+     */
+    public static function add($cookie)
+    {
+        if (is_array($cookie)) {
+            $cookie = new Cookie($cookie);
+        }
+
+        Yii::$app->response->cookies->add($cookie);
+    }
+
+    /**
+     * Returns whether there is a cookie with the specified name.
+     * Note that if a cookie is marked for deletion from browser, this method will return false.
+     * @param string $name
+     * @return bool
+     */
+    public static function has($name)
+    {
+        return Yii::$app->response->cookies->has($name);
+    }
+
+    /**
+     * Get cookie object
+     * @param string $name
+     * @return Cookie
+     */
+    public static function get($name)
+    {
+        return Yii::$app->response->cookies->get($name);
+    }
+
+    /**
+     * Get cookie value
+     * @param string $name
+     * @param null $defaultValue
+     * @return mixed
+     */
+    public static function getValue($name, $defaultValue = null)
+    {
+        return Yii::$app->response->cookies->getValue($name, $defaultValue);
+    }
+
+    /**
+     * Get count cookies
+     * @return int
+     */
+    public static function count()
+    {
+        return Yii::$app->response->cookies->getCount();
+    }
+
+    /**
+     * Remove cookie by name
+     * @param string $name
+     * @param bool $removeFromBrowser
+     */
+    public static function remove($name, $removeFromBrowser = true)
+    {
+        Yii::$app->response->cookies->remove($name, $removeFromBrowser);
+    }
+
+    /**
+     * Remove all cookies
+     */
+    public static function removeAll()
+    {
+        Yii::$app->response->cookies->removeAll();
+    }
+
+    /**
+     * Get cookie as array
+     * @return array
+     */
+    public static function toArray()
+    {
+        return Yii::$app->response->cookies->toArray();
+    }
+
+    /**
+     * Set cookies from array
+     * @param array $value
+     */
+    public static function fromArray(array $value)
+    {
+        Yii::$app->response->cookies->fromArray($value);
+    }
+}
