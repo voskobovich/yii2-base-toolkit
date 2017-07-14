59d0c3133cb0fc98a6ec3ac85bbb6f04730fa381
Vitaly Voskobovich <raficone@gmail.com>
2017-07-14 15:46:31 +0300

    Dev

--- src/db/ActiveRecord.php
+++ src/db/ActiveRecord.php
@@ -4,10 +4,7 @@ namespace voskobovich\base\db;
 
 use voskobovich\base\interfaces\ModelInterface;
 use voskobovich\base\traits\ModelTrait;
-use Yii;
 use yii\base\ErrorException;
-use yii\db\ActiveQuery;
-use yii\helpers\ArrayHelper;
 
 /**
  * Class ActiveRecord.
@@ -77,85 +74,5 @@ class ActiveRecord extends \yii\db\ActiveRecord implements ModelInterface
         }
 
         return str_replace(strrchr(static::class, '\\'), '', static::class) . '\\' . $classShortName;
-    }
-
-    /**
-     * Query for building list for DropDown.
-     *
-     * @param string $keyField
-     * @param string $valueField
-     * @param bool $asArray
-     *
-     * @return ActiveQuery
-     */
-    public static function listAllQueryBuilder($keyField, $valueField, $asArray): ActiveQuery
-    {
-        $query = static::find();
-
-        if ($asArray) {
-            $query->select([$keyField, $valueField])->asArray();
-        }
-
-        return $query;
-    }
-
-    /**
-     * Array for DropDownList.
-     *
-     * @param string $keyField
-     * @param string $valueField
-     * @param bool $asArray
-     *
-     * @return array
-     */
-    public static function listAll($keyField = 'id', $valueField = 'name', $asArray = false): array
-    {
-        $query = static::listAllQueryBuilder($keyField, $valueField, $asArray);
-
-        return ArrayHelper::map(
-            $query->all(),
-            $keyField,
-            $valueField
-        );
-    }
-
-    /**
-     * Formatted date create record.
-     *
-     * @param string $format
-     * @param string $emptyLabel
-     *
-     * @throws \yii\base\InvalidParamException
-     * @throws \yii\base\InvalidConfigException
-     *
-     * @return string
-     */
-    public function getCreated($format = 'long', $emptyLabel = '-'): string
-    {
-        if (empty($this->created_at)) {
-            return $emptyLabel;
-        }
-
-        return Yii::$app->formatter->asDate($this->created_at, $format);
-    }
-
-    /**
-     * Formatted date update record.
-     *
-     * @param string $format
-     * @param string $emptyLabel
-     *
-     * @throws \yii\base\InvalidParamException
-     * @throws \yii\base\InvalidConfigException
-     *
-     * @return string
-     */
-    public function getUpdated($format = 'long', $emptyLabel = '-'): string
-    {
-        if (empty($this->updated_at)) {
-            return $emptyLabel;
-        }
-
-        return Yii::$app->formatter->asDate($this->updated_at, $format);
     }
 }
